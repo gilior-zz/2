51a2decc1a4812e810998f6203773ce011f648e5
Gishry, Lior (lg5712) <lg5712@intl.att.com>
2021-01-05 23:18:38 +0200

    more

---
+++ src/collections/collections.ts
@@ -0,0 +1,12 @@
+import template from './many-controls.html'
+import * as ko from 'knockout'
+import {Observable, ObservableArray} from "knockout";
+
+class ManyControls {
+
+    constructor() {
+    }
+
+}
+
+export default {viewModel: ManyControls, template: template}
