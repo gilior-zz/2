0e7f79a29ae8f276c6a9e5518dbe4652196c0ae5
Gishry, Lior (lg5712) <lg5712@intl.att.com>
2021-01-06 13:16:56 +0200

    more

--- src/collections/collections.ts
+++ src/collections/collections.ts
@@ -1,12 +1,23 @@
-import template from './many-controls.html'
+import template from './collections.html'
 import * as ko from 'knockout'
 import {Observable, ObservableArray} from "knockout";
 
-class ManyControls {
+class Collections {
+    parents: ObservableArray<{ name: Observable<string>, children: ObservableArray<string> }> = ko.observableArray<{ name: Observable<string>, children: ObservableArray<string> }>([
+        {name: ko.observable("Annabelle "), children: ko.observableArray()},
+        {name: ko.observable("Charles "), children: ko.observableArray()},
+        {name: ko.observable("Bertie "), children: ko.observableArray()},
+    ])
 
     constructor() {
     }
 
+    on_add_child = (data:any, event:any) => {
+        var index = event.target.getAttribute("index")
+
+        this.parents()[index].children.push("New child")
+    }
+
 }
 
-export default {viewModel: ManyControls, template: template}
+export default {viewModel: Collections, template: template}
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ import Counter from './counter/counter'
 import SimpleList from './simple_list/simple-list'
 import SmartList from './smart-list/smart-list'
 import ManyControls from './many-controls/many-controls'
+import Collections from "./collections/collections";
 ko.components.register('children',Children);
 ko.components.register('hello',Hello);
 ko.components.register('counter',Counter);
 ko.components.register('simple-list',SimpleList);
 ko.components.register('smart-list',SmartList);
 ko.components.register('many-controls',ManyControls);
+ko.components.register('collections',Collections);
 class Person{
     name:Observable<string>=ko.observable("lior");
     constructor() {
